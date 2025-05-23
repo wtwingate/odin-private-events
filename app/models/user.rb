07c4ac147b29a599f2ec59f601949cdc3cc3ad28
William Wingate <wtwingate@gmail.com>
2025-05-23 13:42:38 -0400

    Create method to get User's full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: "creator_id", class_name: "Event"
   has_many :attendances, foreign_key: "attendee_id"
   has_many :attended_events, through: :attendances, source: :event
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
