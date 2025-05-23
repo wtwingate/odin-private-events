4d845d54178d91eef5e4bb41b88883afdf3def55
William Wingate <wtwingate@gmail.com>
2025-05-23 12:41:06 -0400

    Allow registered users to create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,31 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
+
   def index
     @events = Event.all
   end
 
   def show
     @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event was successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.expect(event: [ :name, :date ])
   end
 end
