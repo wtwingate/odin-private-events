96e9f45f12838887a4f00112005c0037a44bba91
William Wingate <wtwingate@gmail.com>
2025-05-27 20:29:16 -0400

    Create class methods for past and upcoming events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,8 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
 
   def index
-    @events = Event.all
+    @upcoming_events = Event.upcoming_events
+    @past_events = Event.past_events
   end
 
   def show
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,15 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendances
   has_many :attendees, through: :attendances
+
+  validates :name, presence: true
+  validates :date, presence: true
+
+  def self.upcoming_events
+    where(date: Date.today..)
+  end
+
+  def self.past_events
+    where(date: ...Date.today)
+  end
 end
