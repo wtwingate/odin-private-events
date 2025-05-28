e6210faa38117c0f8d5db284784ea895a00d6f73
William Wingate <wtwingate@gmail.com>
2025-05-27 20:09:12 -0400

    Prevent creators from RSVPing to their own events

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,4 +3,13 @@ class Attendance < ApplicationRecord
   belongs_to :attendee, class_name: "User"
 
   validates :attendee_id, uniqueness: { scope: :event_id, message: "is already registered for this event" }
+  validate :attendee_cannot_be_creator
+
+  private
+
+  def attendee_cannot_be_creator
+    if event.creator_id == attendee.id
+      errors.add(:attendee_id, "cannot be the creator of the event")
+    end
+  end
 end
